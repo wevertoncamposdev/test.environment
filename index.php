5ef5246100204a9bba5519249a818ea65a369487
wevertoncamposdev <wevertoncamposdev@gmail.com>
2022-06-19 02:27:58 -0300

    tratando os dados

--- index.php
+++ index.php
@@ -1,5 +1,23 @@
+<?php
+
+/** PORTAL DA TRANSPARÊNCIA
+ * https://portaldatransparencia.gov.br/api-de-dados/cadastrar-email
+ * https://transparencia.gov.br/
+ * 
+ * 
+ * https://portaldatransparencia.gov.br/api-de-dados
+ * 
+ * 
+ * https://api.portaldatransparencia.gov.br/swagger-ui.html
+ * 
+ * Acesso a API: {"key":"chave-api-dados","value":"b2ac369138bd866ba9b0a92f31bced84"}
+ * 
+ */
+
+?>
+
 <!DOCTYPE html>
-<html lang="pt">
+<html lang="pt-BR">
 
 <head>
     <meta charset="utf-8" />
@@ -12,52 +30,55 @@
     <link href="assets/css/vendor/jquery-jvectormap-1.2.2.css" rel="stylesheet" type="text/css" />
     <!-- third party css end -->
 
+    <!-- third party css -->
+    <link href="assets/css/vendor/dataTables.bootstrap5.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/vendor/responsive.bootstrap5.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/vendor/buttons.bootstrap5.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/vendor/select.bootstrap5.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/vendor/fixedHeader.bootstrap5.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/vendor/fixedColumns.bootstrap5.css" rel="stylesheet" type="text/css" />
+    <!-- third party css end -->
+
+    <!-- third party css -->
+    <link href="assets/css/vendor/britecharts.min.css" rel="stylesheet" type="text/css" />
+
+    <!-- Datatables css -->
+    <link href="assets/css/vendor/buttons.bootstrap5.css" rel="stylesheet" type="text/css" />
+
     <!-- App css -->
     <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />
     <link href="assets/css/app.min.css" rel="stylesheet" type="text/css" id="app-style" />
+
 
 </head>
 
 <body class="show" data-layout-color="light" data-layout-mode="fluid" data-rightbar-onstart="true" data-leftbar-theme="dark">
 
     <section class="container mt-5">
-        <h3>PESQUISA DADOS SOBRE CORONAVIRUS NO PORTAL DA TRANSPARÊNCIA DO GOVERNO</h3>
+        <h3 class="text-center">PESQUISA DE DADOS SOBRE CORONAVIRUS NO PORTAL DA TRANSPARÊNCIA DO GOVERNO</h3>
         <hr>
         <form method="post" action='/' class="d-flex justify-content-center">
             <div class="row d-flex justify-content-center">
-                <div class="col-6">
-                    <input class="form-control" type="text" name="date" placeholder="AAAAMM">
+                <div class="col-6 mb-3">
+                    <input class="form-control" type="text" name="info[]" placeholder="AAAAMM">
                     <label for="date" class="">Exemplo: 202101</label>
                 </div>
-                <div class="col-6">
-                    <input class="form-control" type="number" value="" name="page" placeholder="Qtd de Página">
-                    <label for="date" class="">Exemplo: 1, 10 ou 100 páginas</label>
+                <div class="col-6 mb-3">
+                    <input class="form-control" type="number" value="" name="info[]" placeholder="Qtd de Página">
+                    <label for="date" class="">Exemplo: 1, 10 ou 100</label>
                 </div>
                 <input class="form-control btn btn-success w-25" type="submit">
             </div>
 
         </form>
+        <hr>
 
         <?php
 
-        /** PORTAL DA TRANSPARÊNCIA
-         * https://portaldatransparencia.gov.br/api-de-dados/cadastrar-email
-         * https://transparencia.gov.br/
-         * 
-         * 
-         * https://portaldatransparencia.gov.br/api-de-dados
-         * 
-         * 
-         * https://api.portaldatransparencia.gov.br/swagger-ui.html
-         * 
-         * Acesso a API: {"key":"chave-api-dados","value":"b2ac369138bd866ba9b0a92f31bced84"}
-         * 
-         */
-        if (empty($_POST['date'])) {
-            die();
+        if (empty($_POST['info'][0])) {
         } else {
-            $date = $_POST['date'];
-            $page = $_POST['page'];
+            $date = $_POST['info'][0];
+            $page = $_POST['info'][1];
 
             for ($i = 1; $i <= $page; $i++) {
 
@@ -69,24 +90,83 @@
                 curl_setopt($client, CURLOPT_RETURNTRANSFER, true);
 
                 $response = curl_exec($client);
-                $result = json_decode($response);
-        ?>
-                <hr>
-                <h3>RESULTADOS</h3>
-
-        <?php
-                foreach ($result as $row) {
-
-                    echo ('<pre>');
-                    print_r($row);
-                    echo ('</pre>');
-                }
-
-                //curl_close($client);
+                $result = json_decode($response, true);
+                $json_result = json_encode($result);
             }
         }
         ?>
+
+        <div class="d-flex justify-content-center">
+            <h3>RESULTADOS</h3>
+        </div>
+
+        <div class="tab-content mb-5 mt-3">
+
+            <div class="tab-pane show active" id="basic-datatable-preview">
+                <table id="basic-datatable" class="table table-striped dt-responsive nowrap w-100">
+                    <thead>
+                        <tr>
+
+                            <?php
+
+                            if (isset($result)) {
+                                $row = array();
+                                foreach ($result as $row) {
+                                    $row = $row;
+                                }
+
+                                foreach ($row as $key => $val) {
+                                    echo ("<th>$key</th>");
+                                }
+                                //curl_close($client);
+                            }
+                            ?>
+
+                        </tr>
+                    </thead>
+
+                    <tbody>
+
+                        <?php
+                        if (isset($result)) {
+                            foreach ($result as $row) {
+                                echo "<tr>";
+                                foreach ($row as $key => $val) {
+                                    echo ("<td>$val</td>");
+                                }
+                                echo "</tr>";
+                            }
+                            curl_close($client);
+                        }
+
+                        ?>
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </section>
+
+    <section class="container mb-5">
+        <div class="row">
+            <div class="col-xl-12">
+                <div class="card">
+                    <div class="card-body">
+                        <div dir="ltr">
+                            <div id="basic-bar" class="apex-charts" data-colors="#39afd1"></div>
+                        </div>
+                    </div>
+                    <!-- end card body-->
+                </div>
+                <!-- end card -->
+            </div>
+        </div>
+    </section>
+
+
+
+
+
     <!-- bundle -->
     <script src="assets/js/vendor.min.js"></script>
     <script src="assets/js/app.min.js"></script>
@@ -99,6 +179,70 @@
 
     <!-- Dropzone js -->
     <script src="assets/js/vendor/dropzone.js"></script>
+
+    <!-- third party js -->
+    <script src="assets/js/vendor/jquery.dataTables.min.js"></script>
+    <script src="assets/js/vendor/dataTables.bootstrap5.js"></script>
+    <script src="assets/js/vendor/dataTables.responsive.min.js"></script>
+    <script src="assets/js/vendor/responsive.bootstrap5.min.js"></script>
+    <script src="assets/js/vendor/dataTables.buttons.min.js"></script>
+    <script src="assets/js/vendor/buttons.bootstrap5.min.js"></script>
+    <script src="assets/js/vendor/buttons.html5.min.js"></script>
+    <script src="assets/js/vendor/buttons.flash.min.js"></script>
+    <script src="assets/js/vendor/buttons.print.min.js"></script>
+    <script src="assets/js/vendor/dataTables.keyTable.min.js"></script>
+    <script src="assets/js/vendor/dataTables.select.min.js"></script>
+    <script src="assets/js/vendor/fixedColumns.bootstrap5.min.js"></script>
+    <script src="assets/js/vendor/fixedHeader.bootstrap5.min.js"></script>
+    <!-- third party js ends -->
+
+    <!-- demo app -->
+    <script src="assets/js/pages/demo.datatable-init.js"></script>
+    <!-- end demo js-->
+
+    <!-- third party:js -->
+    <script src="assets/js/vendor/apexcharts.min.js"></script>
+    <!-- <script src="assets/js/pages/demo.apex-bar.js"></script> -->
+    <!-- third party end -->
+
+    <script>
+        console.log(<?= $json_result ?>)
+        let result = <?= $json_result ?>;
+
+        let valor = []
+        let data = []
+        result.forEach((value) => {
+            valor.push(value['favorecido'])
+            data.push(value['valor'])
+        })
+
+        console.log(valor);
+        console.log(data);
+
+        let el = document.getElementById('basic-bar');
+        let optinons = {
+            chart: {
+                type: 'bar',
+            },
+            plotOptions: {
+                bar: {
+                    horizontal: true
+                }
+            },
+            series: [{
+                name: 'Valor',
+                data: data
+            }],
+            xaxis: {
+                categories: valor
+            },
+            title: {
+                text: 'VALORES'
+            }
+        }
+        let chart = new ApexCharts(el, optinons);
+        chart.render();
+    </script>
 
 
 
